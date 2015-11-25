2991222fd5b2e9da2896794a7a5a8caea284b91d
Engin Kurutepe <engin@kurutepe.com>
2015-11-25 17:19:35 +0100

    carthage support

---
+++ Source/OCMockito-iOS/OCMockito-iOS.h
@@ -0,0 +1,19 @@
+//
+//  OCMockito-iOS.h
+//  OCMockito-iOS
+//
+//  Created by Engin Kurutepe on 25/11/15.
+//  Copyright © 2015 Jonathan M. Reid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OCMockito-iOS.
+FOUNDATION_EXPORT double OCMockito_iOSVersionNumber;
+
+//! Project version string for OCMockito-iOS.
+FOUNDATION_EXPORT const unsigned char OCMockito_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OCMockito_iOS/PublicHeader.h>
+
+
---
+++ Source/OCMockito-iOSTests/OCMockito_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  OCMockito_iOSTests.m
+//  OCMockito-iOSTests
+//
+//  Created by Engin Kurutepe on 25/11/15.
+//  Copyright © 2015 Jonathan M. Reid. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OCMockito_iOSTests : XCTestCase
+
+@end
+
+@implementation OCMockito_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
